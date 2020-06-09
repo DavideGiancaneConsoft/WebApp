c8a52d161f1ae5208fbd1a96296660394b607d9f
Davide Giancane <davide.giancane@consoft.it>
2020-06-09 20:06:42 +0200

    First Commit

---
+++ WebContent/ShowCustomer.jsp
@@ -0,0 +1,21 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Customer details</title>
+</head>
+
+<body>
+<label>Name: </label>
+${customer.firstName} <br>
+
+<label>Last name: </label>
+${customer.lastName} <br>
+
+<label>Phone number: </label>
+${customer.phoneNumber}
+</body>
+
+</html>
---
+++ src/com/webapp/servlet/Customer.java
@@ -0,0 +1,43 @@
+package com.webapp.servlet;
+
+import java.io.Serializable;
+
+public class Customer implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	private String firstName, lastName, phoneNumber;
+	
+	public Customer() {}
+	
+	public Customer(String firstName, String lastName, String phoneNumber) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.phoneNumber = phoneNumber;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+	
+	
+}
