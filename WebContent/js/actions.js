6c9aeecf8be5c05d7b707dfc2b8c4530f8934bcc
Davide Giancane <davide.giancane@consoft.it>
2020-06-19 09:26:19 +0200

    Corretto errore su id filtro di ricerca in actions.js

--- WebContent/js/actions.js
+++ WebContent/js/actions.js
@@ -7,7 +7,7 @@ function filterList() {
   var input, filter, ul, li, a, i, txtValue;
   input = document.getElementById('myInput');
   filter = input.value.toUpperCase();
-  ul = document.getElementById("myUL");
+  ul = document.getElementById("myCustomers");
   li = ul.getElementsByTagName('li');
 
   // Loop through all list items, and hide those who don't match the search query
