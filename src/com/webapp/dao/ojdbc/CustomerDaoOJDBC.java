26619a182471b6dfd47914d44bf1fa2d14e7eb89
Davide Giancane <davide.giancane@consoft.it>
2020-07-06 16:32:58 +0200

    Implementato metodo readCustomers con main

--- src/com/webapp/dao/ojdbc/CustomerDaoOJDBC.java
+++ src/com/webapp/dao/ojdbc/CustomerDaoOJDBC.java
@@ -1,7 +1,9 @@
 package com.webapp.dao.ojdbc;
 
+import java.sql.CallableStatement;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Collection;
 
@@ -10,13 +12,14 @@ import com.webapp.dao.DBPropertiesManager;
 import com.webapp.dao.DaoException;
 import com.webapp.dao.ICustomerDAO;
 
+import oracle.jdbc.OracleTypes;
+
 public class CustomerDaoOJDBC implements ICustomerDAO{
 	
 	private static CustomerDaoOJDBC instance;
-	
 	private Connection connection;
-	
 	private DBPropertiesManager dbPropertiesManager;
+	private CallableStatement stmt;
 
 	private CustomerDaoOJDBC() {}
 	
@@ -28,42 +31,84 @@ public class CustomerDaoOJDBC implements ICustomerDAO{
 		return instance;
 	}
 	
+	/**
+	 * Richiama la procedura dichiarata nel server di Oracle DB
+	 * che ritorna tutti i customer registrati
+	 */
 	@Override
 	public Collection<Customer> readCustomers() throws DaoException {
-		// TODO Auto-generated method stub
+		ResultSet rs = null;
+		try {
+			//Apro la connessione e preparo lo statement che richiama la procedura "get_all_customers"
+			openNewConnection();
+			stmt = connection.prepareCall("{call customer_package.get_all_customers(?)}");
+			
+			//Imposto il tipo del parametro della procedura (che è di ritorno), ovvero un CURSORE
+			stmt.registerOutParameter(1, OracleTypes.CURSOR);
+			
+			//Eseguo lo statement
+			stmt.execute();
+			
+			//Prendo il cursore e lo casto a result set per poterlo analizzare lato java
+			rs = (ResultSet) stmt.getObject(1);
+			
+			while(rs.next()) {
+				System.out.println("Customer " + rs.getInt("cust_id"));
+				System.out.println("- Name: " + rs.getString("first_name"));
+				System.out.println("- Last name: " + rs.getString("last_name"));
+				System.out.println("- Phone: " + rs.getString("phone"));
+				System.out.println("- City: " + rs.getString("city"));
+			}
+			
+		} catch (SQLException e) {
+			System.err.println("Error: " + e.getMessage());
+			throw new DaoException("Something wrong reading customers with OracleDB");
+		} finally {
+			try {
+				rs.close();
+				stmt.close();
+				connection.close();
+			} catch (SQLException e) {
+				System.err.println("Error in CustomerDaoOJDBC -> readCustomers(): " + e.getMessage());
+			} catch (NullPointerException ignored) {}
+		}
 		return null;
 	}
 
+	/**
+	 * Richiama la procedura dichiarata nel server di Oracle DB
+	 * che inserisce un nuovo customer nel DB
+	 */
 	@Override
 	public void insertCustomer(Customer c) throws DaoException {
 		// TODO Auto-generated method stub
 		
 	}
 
+	/**
+	 * Richiama la procedura dichiarata nel server di Oracle DB
+	 * che elimina un customer esistente dato un DB
+	 */
 	@Override
 	public void deleteCustomer(int id) throws DaoException {
 		// TODO Auto-generated method stub
 		
 	}
 	
-	public void run() throws SQLException {
-		connection = DriverManager.getConnection(dbPropertiesManager.getOracleDbUri(),
-				dbPropertiesManager.getOracleDbUser(), dbPropertiesManager.getOracleDbPsw());
-		if(connection != null)
-			System.out.println("Connected to db");
-		else
-			System.out.println("Fail to make connection");
-	}
-
-	
-	public static void main(String[] args) {
-		CustomerDaoOJDBC dao = CustomerDaoOJDBC.getInstance();
+	private void openNewConnection() {
+		String oracleDbUri = dbPropertiesManager.getOracleDbUri();
+		String oracleDbUser = dbPropertiesManager.getOracleDbUser();
+		String oracleDbPsw =  dbPropertiesManager.getOracleDbPsw();
 		try {
-			dao.run();
+			connection = DriverManager.getConnection(oracleDbUri, oracleDbUser, oracleDbPsw);
 		} catch (SQLException e) {
 			System.err.format("SQL State: %s\n%s", e.getSQLState(), e.getMessage());
 			e.printStackTrace();
 		}
 	}
-
+	
+	public static void main(String[] args) throws DaoException {
+		CustomerDaoOJDBC dao = CustomerDaoOJDBC.getInstance();
+		dao.readCustomers();
+	}
 }
